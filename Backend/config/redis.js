b312d07788e0147eb0eb51957f8d9b01cf1c69fc
Subhadeep <neurondey2017@gmail.com>
2025-07-25 13:44:55 +0530

    Fix Redis multiple connect issue

--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -1,12 +1,18 @@
-const {createClient} = require('redis');
+const { createClient } = require('redis');
 
 const redisClient = createClient({
-    username: 'default',
-    password: process.env.REDIS_KEY, // Ensure you set this in your environment variables
-    socket: {
-        host: 'redis-15272.crce182.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 15272
-    }
+  url: `redis://default:${process.env.REDIS_KEY}@redis-15272.crce182.ap-south-1-1.ec2.redns.redis-cloud.com:15272`
 });
+
+redisClient.on('error', (err) => console.error('Redis connection error:', err));
+
+(async () => {
+  try {
+    await redisClient.connect();
+    console.log('✅ Redis connected successfully');
+  } catch (err) {
+    console.error('❌ Redis connection failed:', err);
+  }
+})();
 
 module.exports = redisClient;
